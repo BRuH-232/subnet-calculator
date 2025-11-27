0b045c7cc291a6858806983e32f10c29001215e9
BRuH-232 <dimavaluev771@gmail.com>
2025-11-27 13:11:46 +0300

    added UiField

---
+++ src/components/UiField.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    interface IProps{
+        label: string
+    }
+
+    const props = defineProps<IProps>();
+</script>
+
+<template>
+    <div class="text-field">
+        {{ props.label }}
+    </div>
+    <slot />
+</template>
+
+<style>
+    .text-field{
+        display: flex;
+        height: 5px;
+        width: auto;
+    }
+</style>
